31b50b0b64069cfe45a14735ccef6fd031892138
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-17 02:51:48 +0300

    Make algo working

--- src/main/kotlin/ru/emkn/kotlin/main.kt
+++ src/main/kotlin/ru/emkn/kotlin/main.kt
@@ -1,42 +1,37 @@
 package ru.emkn.kotlin
+import java.io.File
 import java.util.*
 
-val scan = Scanner(System.`in`)
+//val scan = Scanner(System.`in`)
 fun main(args: Array<String>) {
-    input()
-    //println("Hello with args: " + args.joinToString(", ", "[", "]"))
-}
-fun input() {
-    var inpString = scan.nextLine() //Изменяемое слово
-    val amountStrings = scan.nextInt() //Количество правил
-    val rules = Array(amountStrings) {""} //Массив строк - правил
-
-    for (i in 0 until amountStrings) {  //
-        rules[i] = scan.nextLine()          //  Считывание словаря правил
-    }                                      //
+    var rules = readFileAsLinesUsingUseLines("input.txt") //Считываем набор строк из файла
+    var inpString = rules[0] //Отделяем строку, с которой будем работать
+    rules = rules.drop(1) //Удаляем ее из List
 
     print("$inpString ->  ") //Вывод для отслеживания процесса выполнения алгоритма
 
     var endAlgo = false //Индикатор, нужно ли завершать алгоритм
+
     while (!endAlgo) {
-        inpString = goThroughRules(inpString, rules).first //Обновляем слово
         endAlgo = goThroughRules(inpString, rules).second
+        inpString = goThroughRules(inpString, rules).first //Обновляем слово
         if (endAlgo)
             print(inpString)
         else
             print("$inpString ->  ")
     }
 }
-fun goThroughRules (inpString: String, rules: Array<String>) : Pair<String, Boolean> {
+fun goThroughRules (inpString: String, rules: List<String>) : Pair<String, Boolean> {
     var endAlgo = true
     for (rule in rules) {
         val line = rule.split(" ")
         if(inpString.contains(line[0])){
-            inpString.replaceFirst(line[0], line[2])
             if (!line[1].contains("."))
                 endAlgo = false
-            return Pair(inpString, endAlgo)
+            return Pair(inpString.replaceFirst(line[0], line[2]), endAlgo)
         }
     }
     return Pair("Алгоритм завершает выполнение", true)
 }
+fun readFileAsLinesUsingUseLines(fileName: String): List<String>
+        = File(fileName).useLines { it.toList() }
