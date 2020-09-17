f29d0abc5a41b227fa631b603040426645657888
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-17 10:27:07 +0300

    Add input check

--- src/main/kotlin/ru/emkn/kotlin/main.kt
+++ src/main/kotlin/ru/emkn/kotlin/main.kt
@@ -8,6 +8,11 @@ fun main(args: Array<String>) {
     var inpString = rules[0] //Отделяем строку, с которой будем работать
     rules = rules.drop(1) //Удаляем ее из List
 
+    if(!checkInput(inpString, rules)) {
+        println("Входные данные некорректны. Прочитайте документацию в файле DOC.md")
+        return
+    }
+
     print("$inpString ->  ") //Вывод для отслеживания процесса выполнения алгоритма
 
     var endAlgo = false //Индикатор, нужно ли завершать алгоритм
@@ -31,7 +36,21 @@ fun goThroughRules (inpString: String, rules: List<String>) : Pair<String, Boole
             return Pair(inpString.replaceFirst(line[0], line[2]), endAlgo)
         }
     }
-    return Pair("Алгоритм завершает выполнение", true)
+    return Pair(inpString, true)
 }
 fun readFileAsLinesUsingUseLines(fileName: String): List<String>
         = File(fileName).useLines { it.toList() }
+
+fun checkInput(inpString: String, rules: List<String>) : Boolean {
+    var inputCorrect = true
+    if(!inpString.all { it.isLetter() || it.isDigit() }) { inputCorrect = false }
+
+    for (rule in rules) {
+        val temp = rule.trim().split(" ")
+        if(temp.size != 3)
+            inputCorrect = false
+        if(!temp[0].all { it.isLetter() || it.isDigit() }) { inputCorrect = false }
+        if(!temp[2].all { it.isLetter() || it.isDigit() }) { inputCorrect = false }
+    }
+    return(inputCorrect)
+}
